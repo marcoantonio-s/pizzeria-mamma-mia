ebad5143f2d9d4bbe3b7857794d352da21d1de90
marcoantonio-s <m.sierraortega@gmail.com>
2026-04-17 21:09:34 -0400

    mod uno

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import headerImg from "../assets/img/header.jpg";
+
 function Header () {
     return (
             <section className="d-flex align-items-center justify-content-center text-center text-white"
                 style={{
-                    backgroundImage:"linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url('./src/assets/img/header.jpg')",
+                    backgroundImage: `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url(${headerImg})`,
                     backgroundSize: "cover",
                     backgroundPosition: "center",
                     minHeight: "40vh",
